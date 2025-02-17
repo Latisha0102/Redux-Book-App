ed49b45b47eca54c334f77baa72d9b53be2ed7bc
Your Name <latishamehra5@gmail.com>
2025-02-17 23:38:32 +0530

    adding vercel

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.delete("/books/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
